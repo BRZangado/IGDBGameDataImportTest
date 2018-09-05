058f4370be02e50baa5bcc8c9310fadfb1ceee76
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2018-09-05 11:01:00 -0300

    Deleted imports

--- testeRest/importdata/views.py
+++ testeRest/importdata/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import IGDBGame, Genre
-from .serializers import IGDBGameSerializer, IGDBGenreSerializer
 
 
 class IgDBView(APIView):
